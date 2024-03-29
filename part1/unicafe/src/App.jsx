43cdc6ff553a1a2c146302b2698358c26e6dd641
Pau Antúnez <pau.antunez.clavero@gmail.com>
2024-03-29 13:48:25 +0100

    exercice 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,56 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.total === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>all: {props.total}</div>;
+};
+
+const Stats = ({ total, score, positivePercentage }) => {
+  if (total === 0) {
+    return <div>No data to display</div>;
+  }
+
+  const average = score / total;
+
+  return (
+    <div>
+      <p>all: {total}</p>
+      <p>average: {average.toFixed(2)}</p>
+      <p>positive: {positivePercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
+
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
 
   const handleGoodClick = () => {
+    setTotal(total + 1);
     setGood(good + 1);
+    setScore(score + 1);
   };
 
   const handleNeutralClick = () => {
+    setTotal(total + 1);
     setNeutral(neutral + 1);
   };
 
   const handleBadClick = () => {
+    setTotal(total + 1);
     setBad(bad + 1);
+    setScore(score - 1);
   };
+
+  const positivePercentage = (good / total) * 100;
 
   return (
     <div>
@@ -28,6 +61,7 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <Stats total={total} score={score} positivePercentage={positivePercentage} />
     </div>
   );
 };
