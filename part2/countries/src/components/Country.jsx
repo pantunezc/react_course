3a99001aa8c8c616495c9c2dcf95fc3e7b62e7f3
Pau Antúnez <pau.antunez.clavero@gmail.com>
2024-04-01 12:15:48 +0200

    exercice 2.19

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,12 +1,24 @@
+// Country.js
+import React, { useState } from "react";
+
 const Country = ({ countryInfo }) => {
+  const [selectedCountryIndex, setSelectedCountryIndex] = useState(null);
+
+  const handleShowCountryDetails = (index) => {
+    setSelectedCountryIndex(index);
+  };
+
   return (
     <>
       {countryInfo && countryInfo.length > 10 ? (
         <p>Too many matches, please specify your search.</p>
       ) : countryInfo && countryInfo.length > 2 ? (
         <ul>
-          {countryInfo.map((country) => (
-            <li key={country.name.common}>{country.name.common}</li>
+          {countryInfo.map((country, index) => (
+            <li key={country.name.common}>
+              {country.name.common}
+              <button onClick={() => handleShowCountryDetails(index)}>Show</button>
+            </li>
           ))}
         </ul>
       ) : countryInfo && countryInfo.length === 1 ? (
@@ -21,6 +33,21 @@ const Country = ({ countryInfo }) => {
             })}
           </ul>
           <img src={countryInfo[0].flags.png} alt={countryInfo[0].flags.alt} />
+        </div>
+      ) : null}
+
+      {selectedCountryIndex !== null && countryInfo && countryInfo.length > 1 ? (
+        <div>
+          <h2>{countryInfo[selectedCountryIndex].name.common}</h2>
+          <p>Capital: {countryInfo[selectedCountryIndex].capital}</p>
+          <p>Area: {countryInfo[selectedCountryIndex].area}</p>
+          <ul>
+            {Object.keys(countryInfo[selectedCountryIndex].languages).map((languageCode) => {
+              const languageName = countryInfo[selectedCountryIndex].languages[languageCode];
+              return <li key={languageCode}>{languageName}</li>;
+            })}
+          </ul>
+          <img src={countryInfo[selectedCountryIndex].flags.png} alt={countryInfo[selectedCountryIndex].flags.alt} />
         </div>
       ) : null}
     </>
